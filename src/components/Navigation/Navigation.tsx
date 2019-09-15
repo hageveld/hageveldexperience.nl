984b759910f403f3479a7f2628fb695e81603d28
Luit Hollander <luit@hollander.email>
2019-09-15 23:29:02 +0200

    Remove primer style

--- src/components/Navigation/Navigation.tsx
+++ src/components/Navigation/Navigation.tsx
@@ -2,11 +2,9 @@ import React, { FunctionComponent } from 'react';
 import { navigate } from 'gatsby';
 import { Location } from '@reach/router';
 import styled from 'styled-components';
-import { UnderlineNav } from '@primer/components';
+import { Menu, Icon } from 'antd';
 
-const Link = styled(UnderlineNav.Link)`
-    cursor: pointer;
-`;
+const { SubMenu } = Menu;
 
 interface MenuItem {
     name: string;
@@ -18,20 +16,53 @@ interface Props {
     items: MenuItem[];
 }
 
+/*
+{items.map((item: MenuItem, n: number) => (
+    <Link
+        key={n}
+        onClick={() => navigate(item.path)}
+        className={item.match.test(location.pathname) ? 'selected' : ''}
+    >
+        {item.name}
+    </Link>
+))}
+*/
+
 const Navigation: FunctionComponent<Props> = ({ items }: Props) => (
     <Location>
         {({ location }) => (
-            <UnderlineNav>
-                {items.map((item: MenuItem, n: number) => (
-                    <Link
-                        key={n}
-                        onClick={() => navigate(item.path)}
-                        className={item.match.test(location.pathname) ? 'selected' : ''}
-                    >
-                        {item.name}
-                    </Link>
-                ))}
-            </UnderlineNav>
+            <Menu onClick={() => false} selectedKeys={[]} mode="horizontal">
+                <Menu.Item key="mail">
+                    <Icon type="mail" />
+                    Navigation One
+                </Menu.Item>
+                <Menu.Item key="app" disabled={true}>
+                    <Icon type="appstore" />
+                    Navigation Two
+                </Menu.Item>
+                <SubMenu
+                    title={
+                        <span className="submenu-title-wrapper">
+                            <Icon type="setting" />
+                            Navigation Three - Submenu
+                        </span>
+                    }
+                >
+                    <Menu.ItemGroup title="Item 1">
+                        <Menu.Item key="setting:1">Option 1</Menu.Item>
+                        <Menu.Item key="setting:2">Option 2</Menu.Item>
+                    </Menu.ItemGroup>
+                    <Menu.ItemGroup title="Item 2">
+                        <Menu.Item key="setting:3">Option 3</Menu.Item>
+                        <Menu.Item key="setting:4">Option 4</Menu.Item>
+                    </Menu.ItemGroup>
+                </SubMenu>
+                <Menu.Item key="alipay">
+                    <a href="https://ant.design" target="_blank" rel="noopener noreferrer">
+                        Navigation Four - Link
+                    </a>
+                </Menu.Item>
+            </Menu>
         )}
     </Location>
 );
