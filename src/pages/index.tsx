e315b56d4e340770eba56f20541b136e066011fd
Luit Hollander <luit@hollander.email>
2019-09-15 21:16:25 +0200

    Update page title

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,19 @@
 import React, { FunctionComponent } from 'react';
 import Layout from '../components/Layout';
+import styled from 'styled-components';
 
 import '../sass/index.scss';
 
+const Title = styled.div`
+    font-size: 40px;
+    font-family: Georgia;
+    color: #362e95;
+    text-transform: uppercase;
+`;
+
 const Index: FunctionComponent = () => (
     <Layout>
-        <h1>Hageveld</h1>
+        <Title>College Hageveld</Title>
     </Layout>
 );
 
