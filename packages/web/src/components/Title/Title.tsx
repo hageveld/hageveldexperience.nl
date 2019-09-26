03f0e2fac3527ac7144f24da70df9e2018a3c9cc
Luit Hollander <luit@hollander.email>
2019-09-27 00:47:25 +0200

    Fix react/styled-components error

--- packages/web/src/components/Title/Title.tsx
+++ packages/web/src/components/Title/Title.tsx
@@ -2,6 +2,22 @@ import React, { FunctionComponent, ReactNode } from 'react';
 import { Typography } from 'antd';
 import styled from 'styled-components';
 
+/*
+Passing boolean prop fromm React to styled-components doesn't work:
+
+```
+Warning: Received `true` for a non-boolean attribute `centered`.
+
+If you want to write it to the DOM, pass a string instead: centered="true" or centered={value.toString()}.
+```
+
+See: https://github.com/styled-components/styled-components/issues/1198
+*/
+
+interface StyledComponentProps {
+    centered: number;
+}
+
 interface Props {
     children?: ReactNode;
     centered?: boolean;
@@ -10,11 +26,11 @@ interface Props {
 const TitleWrapper = styled(Typography.Title)`
     font-size: 40px;
     color: #362e95 !important;
-    text-align: ${(props: Props) => props.centered ? "center" : "inherit"};
+    text-align: ${(props: StyledComponentProps) => props.centered ? "center" : "inherit"};
 `;
 
 const Title: FunctionComponent<Props> = ({ children, ...props }: Props) => (
-    <TitleWrapper {...props}>{children ? children : "Hageveld Experience"}</TitleWrapper>
+    <TitleWrapper {...props} centered={props.centered ? 1 : 0}>{children ? children : "Hageveld Experience"}</TitleWrapper>
 )
 
 export default Title;
