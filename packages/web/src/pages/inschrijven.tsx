c77072d5beeb103971b804eea6b59461879ea542
Luit Hollander <luit@hollander.email>
2019-10-06 20:49:10 +0200

    Add time lock

--- packages/web/src/pages/inschrijven.tsx
+++ packages/web/src/pages/inschrijven.tsx
@@ -4,13 +4,13 @@ import Title from '../components/Title';
 import { Alert, Row, Col, Card, List } from 'antd';
 import { dagen, activiteiten } from '../data';
 import moment from 'moment';
-import momentLocale from 'moment/locale/nl';
+import 'moment/locale/nl';
 import ActiviteitType from '../classes/activiteit';
 import Activiteit from '../components/Activiteit';
 
 import '../sass/index.scss';
 
-moment.locale('nl', momentLocale);
+moment.locale('nl');
 
 const Inschrijven: FunctionComponent = () => (
     <Layout>
@@ -20,7 +20,7 @@ const Inschrijven: FunctionComponent = () => (
                 <Alert message={<Fragment>Op deze pagina kun je je inschrijven voor de proeflessen.<br /><br /><b>Let op</b>: Je kunt je voor maximaal 2 dagen inschrijven, en maximaal 1 vak per dag.</Fragment>} type="info" showIcon={true} style={{ marginBottom: '20px' }} />
                 <Row gutter={16}>
                     {dagen.map((dag, index) => (
-                        <Col span={8} key={index} xs={24} sm={24} md={24} lg={8}>
+                        <Col span={8} key={index} xs={24} sm={24} md={24} lg={8} style={{ opacity: Date.now() < moment(dag.datum, "DD-MM-YYYY").valueOf() ? 1.0 : 0.3, pointerEvents: Date.now() < moment(dag.datum, "DD-MM-YYYY").valueOf() ? "auto": "none" }}>
                             <Card
                                 title={
                                     <Row>
