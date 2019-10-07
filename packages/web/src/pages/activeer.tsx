2a4f44184e7969766e33a804896aebac0d657e5d
Luit Hollander <luit@hollander.email>
2019-10-07 22:49:40 +0200

    Rework first 2 activate steps

--- packages/web/src/pages/activeer.tsx
+++ packages/web/src/pages/activeer.tsx
@@ -1,8 +1,9 @@
-import React, { FunctionComponent } from 'react';
+import React, { FunctionComponent, Component } from 'react';
 import { Link, navigate } from 'gatsby';
 import Layout from '../components/Layout';
 import Stepper from '../components/Stepper';
-import { Row, Col, Button, Input, Result, Icon, Form, Select, AutoComplete } from 'antd';
+import { Row, Col, Button, Input, Result, Icon, Form, Select, AutoComplete, Tooltip, Alert } from 'antd';
+import { FormComponentProps  } from 'antd/lib/form';
 import Title from '../components/Title';
 import basisscholen from '../../../duo-bo-data/result/result.json';
 import Map from 'pigeon-maps';
@@ -11,6 +12,181 @@ import Marker from 'pigeon-marker';
 const { Option } = Select;
 const { Item } = Form;
 const { Step } = Stepper;
+  
+class Persoonsgegevens extends Component<FormComponentProps> {
+    handleSubmit = e => {
+        e.preventDefault();
+        this.props.form.validateFieldsAndScroll((err, values) => {
+            if (!err) {
+                console.log('Received values of form: ', values);
+            }
+        });
+    };
+  
+    render() {
+        const { getFieldDecorator } = this.props.form;
+  
+        const formItemLayout = {
+            labelCol: {
+                xs: { span: 24 },
+                sm: { span: 4 },
+            },
+            wrapperCol: {
+                xs: { span: 24 },
+                sm: { span: 20 },
+            },
+        };
+
+        const prefixSelector = getFieldDecorator('prefix', {
+            initialValue: '31',
+        })(
+            <Select style={{ width: 70 }}>
+                <Option value="1">+1</Option>
+                <Option value="31">+31</Option>
+            </Select>,
+        );
+  
+        return (
+            <Form {...formItemLayout} onSubmit={this.handleSubmit}>
+                <Form.Item label="Roepnaam">
+                    {getFieldDecorator('roepnaam', {
+                        rules: [{ required: true, message: 'Vul alsjeblieft je roepnaam in.', whitespace: true }],
+                    })(<Input />)}
+                </Form.Item>
+                <Form.Item label="Tussenvoegsel">
+                    {getFieldDecorator('tussenvoegsel', {
+                        rules: [{ whitespace: true }],
+                    })(<Input />)}
+                </Form.Item>
+                <Form.Item label="Achternaam">
+                    {getFieldDecorator('achternaam', {
+                        rules: [{ required: true, message: 'Vul alsjeblieft je achternaam in.', whitespace: true }],
+                    })(<Input />)}
+                </Form.Item>
+                <Form.Item label="Geslacht">
+                    {getFieldDecorator('geslacht', {
+                        rules: [{ required: true, message: 'Vul alsjeblieft je geslacht in.' }],
+                    })(
+                        <Select>
+                            <Option value="M">Jongen</Option>
+                            <Option value="V">Meisje</Option>
+                        </Select>
+                    )}
+                </Form.Item>
+                <Form.Item
+                    label={
+                        <span>
+                            Telefoonnummer&nbsp;
+                            <Tooltip title="Telefoonnummer ouder/verzorger voor calamiteiten">
+                                <Icon type="question-circle-o" />
+                            </Tooltip>
+                        </span>
+                    }
+                >
+                    {getFieldDecorator('telefoonnummer', {
+                        rules: [],
+                    })(<Input addonBefore={prefixSelector} style={{ width: '100%' }} />)}
+                </Form.Item>
+                <Form.Item>
+                    <Button type="primary" htmlType="submit">
+                        Register
+                    </Button>
+                </Form.Item>
+            </Form>
+        );
+    }
+}
+
+class Authenticatie extends Component<FormComponentProps> {
+    state = {
+        confirmDirty: false
+    };
+    
+    handleSubmit = e => {
+        e.preventDefault();
+        this.props.form.validateFieldsAndScroll((err, values) => {
+            if (!err) {
+                console.log('Received values of form: ', values);
+            }
+        });
+    };
+
+    handleConfirmBlur = e => {
+        const { value } = e.target;
+        this.setState({ confirmDirty: this.state.confirmDirty || !!value });
+    };
+    
+    compareToFirstPassword = (rule, value, callback) => {
+        const { form } = this.props;
+        if (value && value !== form.getFieldValue('wachtwoord')) {
+            callback('De wachtwoorden die je hebt ingevult komen niet overeen.');
+        } else {
+            callback();
+        }
+    };
+    
+    validateToNextPassword = (rule, value, callback) => {
+        const { form } = this.props;
+        if (value && this.state.confirmDirty) {
+            form.validateFields(['bevestig_wachtwoord'], { force: true });
+        }
+        callback();
+    };
+  
+    render() {
+        const { getFieldDecorator } = this.props.form;
+  
+        const formItemLayout = {
+            labelCol: {
+                xs: { span: 24 },
+                sm: { span: 4 },
+            },
+            wrapperCol: {
+                xs: { span: 24 },
+                sm: { span: 20 },
+            },
+        };
+  
+        return (
+            <Form {...formItemLayout} onSubmit={this.handleSubmit}>
+                <Form.Item label="Wachtwoord" hasFeedback>
+                    {getFieldDecorator('wachtwoord', {
+                        rules: [
+                            {
+                                required: true,
+                                message: 'Vul alsjeblieft een wachtwoord in.'
+                            },
+                            {
+                                validator: this.validateToNextPassword
+                            }
+                        ]
+                    })(<Input.Password />)}
+                </Form.Item>
+                <Form.Item label="Bevestig wachtwoord" hasFeedback>
+                    {getFieldDecorator('bevestig_wachtwoord', {
+                        rules: [
+                            {
+                                required: true,
+                                message: 'Bevestig alsjeblieft je wachtwoord.'
+                            },
+                            {
+                                validator: this.compareToFirstPassword
+                            }
+                        ]
+                    })(<Input.Password onBlur={this.handleConfirmBlur} />)}
+                </Form.Item>
+                <Form.Item>
+                    <Button type="primary" htmlType="submit">
+                        Register
+                    </Button>
+                </Form.Item>
+            </Form>
+        );
+    }
+}
+  
+const WrappedPersoonsgegevens = Form.create({ name: 'persoonsgegevens' })(Persoonsgegevens);
+const WrappedAuthenticatie = Form.create({ name: 'authenticatie' })(Authenticatie);
 
 const Activeer: FunctionComponent = () => {
     const hash = location.hash.replace("#/", "");
@@ -26,35 +202,12 @@ const Activeer: FunctionComponent = () => {
                     <Col span={12} offset={6}>
                         <Stepper>
                             <Step title="Gegevens" icon="user" description="Persoonsgegevens">
-                                <Form>
-                                    <Item label="Roepnaam">
-                                        <Input />
-                                    </Item>
-                                    <Item label="Tussenvoegsel">
-                                        <Input />
-                                    </Item>
-                                    <Item label="Achternaam">
-                                        <Input />
-                                    </Item>
-                                    <Item label="Geslacht">
-                                        <Select>
-                                            <Option value="M">Jongen</Option>
-                                            <Option value="V">Meisje</Option>
-                                        </Select>
-                                    </Item>
-                                    <Item label="Telefoonnummer">
-                                        <Input />
-                                    </Item>
-                                </Form>
+                                <Alert message="Door het invullen van je persoonsgegevens ga je ermee akkoord dat deze gegevens beveiligd opgeslagen en verwerkt worden." type="info" showIcon />
+                                <br />
+                                <WrappedPersoonsgegevens />
                             </Step>
                             <Step title="Wachtwoord" icon="lock" description="Authenticatie">
-                                <>
-                                    <Input
-                                        prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />}
-                                        type="password"
-                                        placeholder="Password"
-                                    />         
-                                </>
+                                <WrappedAuthenticatie />
                             </Step>
                             <Step title="School" icon="bank" description="Basisschool">
                                 <>
