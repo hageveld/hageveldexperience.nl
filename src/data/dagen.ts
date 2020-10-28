109aa5f46ad74a6b3a261062ab1010eaaf4de7f1
HageveldUser <73248556+HageveldUser@users.noreply.github.com>
2020-10-28 14:41:57 +0100

    Update dagen.ts

--- src/data/dagen.ts
+++ src/data/dagen.ts
@@ -3,25 +3,37 @@ import Dag from '../models/dag';
 const dagen: Dag[] = [
     {
         id: 1,
-        datum: '20-11-2019',
+        datum: '18-11-2020',
         startTijd: '14:00',
         eindTijd: '15:30',
         beschrijving: '(+ rondleiding)'
     },
     {
         id: 2,
-        datum: '27-11-2019',
+        datum: '25-11-2020',
         startTijd: '14:00',
         eindTijd: '15:30',
         beschrijving: '(+ puzzeltocht)'
     },
     {
         id: 3,
-        datum: '08-01-2020',
+        datum: '13-01-2021',
         startTijd: '14:00',
         eindTijd: '15:30',
         beschrijving: '(+ speeddate)'
     }
+     {
+        id: 4,
+        datum: '02-12-2020',
+        startTijd: '14:00',
+        eindTijd: '16:00',
+        beschrijving: 'VIA Experience'
+    }
+        id: 5,
+        datum: '13-01-2021',
+        startTijd: '14:00',
+        eindTijd: '16:00',
+        beschrijving: 'VIA Experience'
 ];
 
 export default dagen;
