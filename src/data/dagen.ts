9750c4325ef96eb237e534ae4919948ab98daa85
Edwin van Leeuwen <edwin@brainstormit.nl>
2020-10-29 18:43:40 +0100

    Update dagen.ts

--- src/data/dagen.ts
+++ src/data/dagen.ts
@@ -21,19 +21,21 @@ const dagen: Dag[] = [
         startTijd: '14:00',
         eindTijd: '15:30',
         beschrijving: '(+ speeddate)'
-    }
-     {
+    },
+    {
         id: 4,
         datum: '02-12-2020',
         startTijd: '14:00',
         eindTijd: '16:00',
         beschrijving: 'VIA Experience'
-    }
+    },
+    {
         id: 5,
         datum: '13-01-2021',
         startTijd: '14:00',
         eindTijd: '16:00',
         beschrijving: 'VIA Experience'
+    }
 ];
 
 export default dagen;
