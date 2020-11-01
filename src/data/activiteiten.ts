f89156646e53d104be3e643b0fd67b3a64de0969
Edwin van Leeuwen <edwin@brainstormit.nl>
2020-11-01 14:40:19 +0100

    Update activiteiten.ts
    Doornummeren ipv wijzigen

--- src/data/activiteiten.ts
+++ src/data/activiteiten.ts
@@ -2,203 +2,383 @@ const activiteiten = [
     {
         id: 1,
         vak: 'AK',
-        dag: 6,
+        dag: 1,
         maxDeelnemers: 30
     },
     {
         id: 2,
-        vak: 'FR',
-        dag: 5,
-        maxDeelnemers: 25
+        vak: 'AK',
+        dag: 3,
+        maxDeelnemers: 30
     },
     {
         id: 3,
         vak: 'BI',
-        dag: 4,
+        dag: 1,
         maxDeelnemers: 20
     },
     {
         id: 4,
         vak: 'BI',
-        dag: 4,
+        dag: 2,
         maxDeelnemers: 20
     },
     {
         id: 5,
         vak: 'BI',
-        dag: 5,
+        dag: 3,
         maxDeelnemers: 20
     },
     {
         id: 6,
-        vak: 'BI',
-        dag: 6,
-        maxDeelnemers: 20
+        vak: 'DU',
+        dag: 2,
+        maxDeelnemers: 30
     },
     {
         id: 7,
         vak: 'EN',
-        dag: 4,
+        dag: 1,
         maxDeelnemers: 24
     },
     {
         id: 8,
         vak: 'EN',
-        dag: 4,
-        maxDeelnemers: 30
+        dag: 2,
+        maxDeelnemers: 24
     },
     {
         id: 9,
         vak: 'EN',
-        dag: 5,
-        maxDeelnemers: 30
+        dag: 3,
+        maxDeelnemers: 24
     },
     {
         id: 10,
-        vak: 'EN',
-        dag: 6,
+        vak: 'GS',
+        dag: 1,
         maxDeelnemers: 30
     },
     {
         id: 11,
         vak: 'GS',
-        dag: 4,
+        dag: 2,
         maxDeelnemers: 30
     },
     {
         id: 12,
         vak: 'GS',
-        dag: 5,
+        dag: 3,
         maxDeelnemers: 30
     },
     {
         id: 13,
-        vak: 'GS',
-        dag: 6,
-        maxDeelnemers: 30
+        vak: 'LA',
+        dag: 1,
+        maxDeelnemers: 24
     },
     {
         id: 14,
         vak: 'LA',
-        dag: 4,
-        maxDeelnemers: 25
+        dag: 2,
+        maxDeelnemers: 24
     },
     {
         id: 15,
-        vak: 'LA',
-        dag: 5,
-        maxDeelnemers: 25
+        vak: 'MU',
+        dag: 3,
+        maxDeelnemers: 30
     },
     {
         id: 16,
         vak: 'NT',
-        dag: 4,
+        dag: 1,
         maxDeelnemers: 20
     },
     {
         id: 17,
         vak: 'NT',
-        dag: 5,
+        dag: 2,
         maxDeelnemers: 20
     },
     {
         id: 18,
         vak: 'NT',
-        dag: 6,
+        dag: 3,
         maxDeelnemers: 20
     },
     {
         id: 19,
         vak: 'SK',
-        dag: 4,
-        maxDeelnemers: 16
+        dag: 1,
+        maxDeelnemers: 24
     },
     {
         id: 20,
         vak: 'SK',
-        dag: 5,
-        maxDeelnemers: 16
+        dag: 2,
+        maxDeelnemers: 24
     },
     {
         id: 21,
         vak: 'SP',
-        dag: 4,
+        dag: 1,
         maxDeelnemers: 30
     },
     {
         id: 22,
         vak: 'SP',
-        dag: 5,
+        dag: 2,
         maxDeelnemers: 30
     },
     {
         id: 23,
         vak: 'SP',
-        dag: 6,
+        dag: 3,
         maxDeelnemers: 30
     },
     {
         id: 24,
         vak: 'TE',
-        dag: 5,
-        maxDeelnemers: 25
+        dag: 2,
+        maxDeelnemers: 24
     },
     {
         id: 25,
         vak: 'TE',
-        dag: 6,
-        maxDeelnemers: 25
+        dag: 3,
+        maxDeelnemers: 24
     },
     {
         id: 26,
         vak: 'WW',
-        dag: 4,
+        dag: 1,
         maxDeelnemers: 30
     },
     {
         id: 27,
         vak: 'WW',
-        dag: 6,
-        maxDeelnemers: 30
+        dag: 3,
+        maxDeelnemers: 24
     },
     {
         id: 28,
         vak: 'WI',
-        dag: 4,
+        dag: 1,
         maxDeelnemers: 30
     },
     {
         id: 29,
         vak: 'WI',
-        dag: 5,
+        dag: 2,
         maxDeelnemers: 30
     },
     {
         id: 30,
         vak: 'WI',
-        dag: 6,
+        dag: 3,
         maxDeelnemers: 30
     },
     {
         id: 31,
+        vak: 'AK',
+        dag: 6,
+        maxDeelnemers: 30
+    },
+    {
+        id: 32,
+        vak: 'FR',
+        dag: 5,
+        maxDeelnemers: 25
+    },
+    {
+        id: 33,
+        vak: 'BI',
+        dag: 4,
+        maxDeelnemers: 20
+    },
+    {
+        id: 34,
+        vak: 'BI',
+        dag: 4,
+        maxDeelnemers: 20
+    },
+    {
+        id: 35,
+        vak: 'BI',
+        dag: 5,
+        maxDeelnemers: 20
+    },
+    {
+        id: 36,
+        vak: 'BI',
+        dag: 6,
+        maxDeelnemers: 20
+    },
+    {
+        id: 37,
+        vak: 'EN',
+        dag: 4,
+        maxDeelnemers: 24
+    },
+    {
+        id: 38,
+        vak: 'EN',
+        dag: 4,
+        maxDeelnemers: 30
+    },
+    {
+        id: 39,
+        vak: 'EN',
+        dag: 5,
+        maxDeelnemers: 30
+    },
+    {
+        id: 40,
+        vak: 'EN',
+        dag: 6,
+        maxDeelnemers: 30
+    },
+    {
+        id: 41,
+        vak: 'GS',
+        dag: 4,
+        maxDeelnemers: 30
+    },
+    {
+        id: 42,
+        vak: 'GS',
+        dag: 5,
+        maxDeelnemers: 30
+    },
+    {
+        id: 43,
+        vak: 'GS',
+        dag: 6,
+        maxDeelnemers: 30
+    },
+    {
+        id: 44,
+        vak: 'LA',
+        dag: 4,
+        maxDeelnemers: 25
+    },
+    {
+        id: 45,
+        vak: 'LA',
+        dag: 5,
+        maxDeelnemers: 25
+    },
+    {
+        id: 46,
+        vak: 'NT',
+        dag: 4,
+        maxDeelnemers: 20
+    },
+    {
+        id: 47,
+        vak: 'NT',
+        dag: 5,
+        maxDeelnemers: 20
+    },
+    {
+        id: 48,
+        vak: 'NT',
+        dag: 6,
+        maxDeelnemers: 20
+    },
+    {
+        id: 49,
+        vak: 'SK',
+        dag: 4,
+        maxDeelnemers: 16
+    },
+    {
+        id: 50,
+        vak: 'SK',
+        dag: 5,
+        maxDeelnemers: 16
+    },
+    {
+        id: 51,
+        vak: 'SP',
+        dag: 4,
+        maxDeelnemers: 30
+    },
+    {
+        id: 52,
+        vak: 'SP',
+        dag: 5,
+        maxDeelnemers: 30
+    },
+    {
+        id: 53,
+        vak: 'SP',
+        dag: 6,
+        maxDeelnemers: 30
+    },
+    {
+        id: 54,
+        vak: 'TE',
+        dag: 5,
+        maxDeelnemers: 25
+    },
+    {
+        id: 55,
+        vak: 'TE',
+        dag: 6,
+        maxDeelnemers: 25
+    },
+    {
+        id: 56,
+        vak: 'WW',
+        dag: 4,
+        maxDeelnemers: 30
+    },
+    {
+        id: 57,
+        vak: 'WW',
+        dag: 6,
+        maxDeelnemers: 30
+    },
+    {
+        id: 58,
+        vak: 'WI',
+        dag: 4,
+        maxDeelnemers: 30
+    },
+    {
+        id: 59,
+        vak: 'WI',
+        dag: 5,
+        maxDeelnemers: 30
+    },
+    {
+        id: 60,
+        vak: 'WI',
+        dag: 6,
+        maxDeelnemers: 30
+    },
+    {
+        id: 61,
         vak: 'MU',
         dag: 4,
         maxDeelnemers: 25
     },
     {
-        id: 32,
+        id: 62,
         vak: 'MU',
         dag: 6,
         maxDeelnemers: 25
     },
     {
-        id: 33,
+        id: 63,
         vak: 'VIA',
         dag: 7,
         maxDeelnemers: 60
     },
     {
-        id: 34,
+        id: 64,
         vak: 'VIA',
         dag: 8,
         maxDeelnemers: 60
