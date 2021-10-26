206a47e685b2d996aeb74577ee130093144c36b2
HageveldUser <73248556+HageveldUser@users.noreply.github.com>
2021-10-26 11:44:47 +0200

    Update activiteiten.ts
    ID's aangepast en activiteiten toegevoegd en dagen aangepast

--- src/data/activiteiten.ts
+++ src/data/activiteiten.ts
@@ -2,139 +2,139 @@ const activiteiten = [
     {
         id: 1,
         vak: 'AK',
-        dag: 1,
+        dag: 3,
         maxDeelnemers: 28
     },
     {
         id: 2,
-        vak: 'AK',
-        dag: 3,
+        vak: 'BI',
+        dag: 1,
         maxDeelnemers: 28
     },
     {
         id: 3,
         vak: 'BI',
-        dag: 1,
+        dag: 2,
         maxDeelnemers: 28
     },
     {
         id: 4,
         vak: 'BI',
-        dag: 2,
+        dag: 3,
         maxDeelnemers: 28
     },
     {
         id: 5,
-        vak: 'BI',
-        dag: 3,
+        vak: 'EN',
+        dag: 1,
         maxDeelnemers: 28
     },
     {
         id: 6,
         vak: 'EN',
-        dag: 1,
+        dag: 3,
         maxDeelnemers: 28
     },
     {
         id: 7,
-        vak: 'EN',
-        dag: 2,
+        vak: 'FR',
+        dag: 1,
         maxDeelnemers: 28
     },
     {
         id: 8,
-        vak: 'EN',
-        dag: 3,
+        vak: 'GS',
+        dag: 1,
         maxDeelnemers: 28
     },
     {
         id: 9,
         vak: 'GS',
-        dag: 1,
+        dag: 2,
         maxDeelnemers: 28
     },
     {
         id: 10,
         vak: 'GS',
-        dag: 2,
+        dag: 3,
         maxDeelnemers: 28
     },
     {
         id: 11,
-        vak: 'GS',
-        dag: 3,
+        vak: 'LA',
+        dag: 2,
         maxDeelnemers: 28
     },
     {
         id: 12,
         vak: 'LA',
-        dag: 1,
+        dag: 3,
         maxDeelnemers: 28
     },
     {
         id: 13,
-        vak: 'LA',
+        vak: 'MU',
         dag: 2,
         maxDeelnemers: 28
     },
     {
         id: 14,
         vak: 'MU',
-        dag: 2,
+        dag: 3,
         maxDeelnemers: 28
     },
     {
         id: 15,
-        vak: 'MU',
-        dag: 3,
+        vak: 'NT',
+        dag: 1,
         maxDeelnemers: 28
     },
     {
         id: 16,
         vak: 'NT',
-        dag: 1,
+        dag: 2,
         maxDeelnemers: 28
     },
     {
         id: 17,
         vak: 'NT',
-        dag: 2,
-        maxDeelnemers: 28
-    },
-    {
-        id: 18,
-        vak: 'NT',
         dag: 3,
         maxDeelnemers: 28
     },
     {
-        id: 19,
+        id: 18,
         vak: 'SK',
         dag: 1,
         maxDeelnemers: 16
     },
     {
-        id: 20,
+        id: 19,
         vak: 'SK',
         dag: 2,
         maxDeelnemers: 16
     },
     {
-        id: 21,
+        id: 20,
         vak: 'SP',
         dag: 1,
+        maxDeelnemers: 28
+    },
+    {
+        id: 21,
+        vak: 'SP',
+        dag: 2,
         maxDeelnemers: 28
     },
     {
         id: 22,
         vak: 'SP',
-        dag: 2,
+        dag: 3,
         maxDeelnemers: 28
     },
     {
         id: 23,
-        vak: 'SP',
-        dag: 3,
+        vak: 'TE',
+        dag: 1,
         maxDeelnemers: 28
     },
     {
@@ -145,14 +145,14 @@ const activiteiten = [
     },
     {
         id: 25,
-        vak: 'TE',
-        dag: 3,
+        vak: 'WW',
+        dag: 1,
         maxDeelnemers: 28
     },
     {
         id: 26,
         vak: 'WW',
-        dag: 1,
+        dag: 2,
         maxDeelnemers: 28
     },
     {
@@ -181,49 +181,49 @@ const activiteiten = [
     },
     {
         id: 31,
-        vak: 'Alfa',
+        vak: 'Alfa: Spel om woordjes te leren',
         dag: 4,
         maxDeelnemers: 16
     },
     {
         id: 32,
-        vak: 'Design Thinking',
+        vak: 'Bèta: proefjes ruimtevaart',
         dag: 4,
         maxDeelnemers: 16
     },
     {
         id: 33,
-        vak: 'Gamma',
+        vak: 'Gamma: De Doos van Pandora',
         dag: 4,
         maxDeelnemers: 16
     },
     {
         id: 34,
-        vak: 'Ruimtevaart',
+        vak: 'Ontwerpen: Duurzame toekomst',
         dag: 4,
         maxDeelnemers: 16
     },
     {
         id: 35,
-        vak: 'Alfa',
+        vak: 'Alfa: Spel om woordjes te leren',
         dag: 5,
         maxDeelnemers: 16
     },
     {
         id: 36,
-        vak: 'Design Thinking',
+        vak: 'Bèta: proefjes ruimtevaart',
         dag: 5,
         maxDeelnemers: 16
     },
     {
         id: 37,
-        vak: 'Gamma',
+        vak: 'Gamma: De Doos van Pandora',
         dag: 5,
         maxDeelnemers: 16
     },
     {
         id: 38,
-        vak: 'Ruimtevaart',
+        vak: 'Ontwerpen: Duurzame toekomst',
         dag: 5,
         maxDeelnemers: 16
     }
